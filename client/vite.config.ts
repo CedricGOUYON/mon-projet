4acd19c49455603a7e458e3cf8d9396a56982088
CédricGOUYON <gouyoncedric@gmail.com>
2025-06-19 13:53:41 +0200

    Patch deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mon-projet/",
+  base: "/mon-projet/", // le nom du repo GitHub exactement ici
   plugins: [react()],
 });
